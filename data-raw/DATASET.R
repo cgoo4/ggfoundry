3ef8c4c19315e48174f40e6cc193722558c597e7
Carl Goodwin <carl.goodwin@quantumjitter.com>
2024-05-22 15:29:32 +0100

    extdata

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -14,6 +14,8 @@ svg_names <- grep(
 svg_paths <- paste0("inst/extdata/", svg_names)
 
 # Convert raw svg to cairo
+# Keep commented out as grConvert not on CRAN
+
 # for (i in svg_paths) {
 #   s <- sub(".svg", "-cairo.svg", i)
 #   if (!file.exists(s)) {
